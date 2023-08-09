7fb304d7dd4f8a433d1d86c273daeb56eff2c714
prasad9055 <101623934+prasad9055@users.noreply.github.com>
2023-08-09 07:54:42 +0530

    Revert "Develop pull request" (#2)

--- src/main/java/com/tesco/enquiry/intg/dao/ICreditLimitEnquiryDao.java
+++ src/main/java/com/tesco/enquiry/intg/dao/ICreditLimitEnquiryDao.java
@@ -1,9 +1,5 @@
 package com.tesco.enquiry.intg.dao;
 
-import com.tesco.enquiry.model.EnquiryDaoRequest;
-import com.tesco.enquiry.model.EnquiryDaoResponse;
-
 public interface ICreditLimitEnquiryDao {
 
-	public EnquiryDaoResponse enquiry(EnquiryDaoRequest enquiryDaoRequest);
 }
--- src/main/java/com/tesco/enquiry/model/EnquiryDaoRequest.java
+++ src/main/java/com/tesco/enquiry/model/EnquiryDaoRequest.java
@@ -1,18 +1,5 @@
 package com.tesco.enquiry.model;
 
-import org.springframework.stereotype.Component;
-
-import lombok.Data;
-
-@Data
-@Component
 public class EnquiryDaoRequest {
-	
-	private String promocode ;
-	private String clientId;
-	private String channelId;
-	private String messageTs;
-	private String requestId;
-
 
 }
