084bd60c949e6767e05aaeed30b35631f87b151e
prasad9055 <101623934+prasad9055@users.noreply.github.com>
2023-08-09 15:05:41 +0530

    project Updated up to encrypt (#10)

--- src/main/java/com/tesco/enquiry/intg/dao/ICreditLimitEnquiryDao.java
+++ src/main/java/com/tesco/enquiry/intg/dao/ICreditLimitEnquiryDao.java
@@ -1,5 +1,9 @@
 package com.tesco.enquiry.intg.dao;
 
+import com.tesco.enquiry.model.EnquiryDaoRequest;
+import com.tesco.enquiry.model.EnquiryDaoResponse;
+
 public interface ICreditLimitEnquiryDao {
 
+	public EnquiryDaoResponse enquiry(EnquiryDaoRequest enquiryDaoRequest);
 }
--- src/main/java/com/tesco/enquiry/service/ICreditLimitEnquiryService.java
+++ src/main/java/com/tesco/enquiry/service/ICreditLimitEnquiryService.java
@@ -1,5 +1,11 @@
 package com.tesco.enquiry.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.tesco.enquiry.model.EnquiryRequest;
+import com.tesco.enquiry.model.EnquiryResponse;
+
 public interface ICreditLimitEnquiryService {
+	
+	public String enquiry(EnquiryRequest enquiryRequest);
 
 }
