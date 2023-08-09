26a760b2fe2b4628b9a47a7585721f1af43f28e5
prasad9055 <101623934+prasad9055@users.noreply.github.com>
2023-08-09 13:14:08 +0530

    update develop (#7)

--- src/main/java/com/tesco/enquiry/CreditlimitEnquiryServiceApplication.java
+++ src/main/java/com/tesco/enquiry/CreditlimitEnquiryServiceApplication.java
@@ -8,6 +8,8 @@ public class CreditlimitEnquiryServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CreditlimitEnquiryServiceApplication.class, args);
+		
+		
 	}
 
 }
